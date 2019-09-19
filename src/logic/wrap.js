3140ce9b35f91b50239f716494121f04cabb322e
Lukas Siemon <lukas.siemon@gmail.com>
2019-09-19 07:38:00 -0700

    feat: added option to ignore invalid event

--- src/logic/wrap.js
+++ src/logic/wrap.js
@@ -1,19 +1,25 @@
+const get = require('lodash.get');
 const request = require('request-promise');
+const Joi = require('joi-strict');
 const { wrap } = require('lambda-async');
 const { logger, abbrev } = require('lambda-monitor-logger');
 
-const submit = async (event, context, success) => {
+const submit = async (event, context, opts) => {
   const missingKeys = [
     'RequestType', 'ServiceToken', 'ResponseURL', 'StackId', 'RequestId',
     'LogicalResourceId', 'ResourceType', 'ResourceProperties'
   ].filter((k) => event[k] === undefined);
   if (missingKeys.length !== 0) {
-    logger.error(`Invalid Event\n${abbrev(event)}`);
-    throw new Error('Invalid custom resource event received');
+    if (opts.errorOnInvalidEvent) {
+      logger.error(`Invalid Event\n${abbrev(event)}`);
+      throw new Error('Invalid custom resource event received');
+    } else {
+      return;
+    }
   }
 
   const requestBody = JSON.stringify({
-    Status: success ? 'SUCCESS' : 'FAILED',
+    Status: opts.success ? 'SUCCESS' : 'FAILED',
     Reason: `See the details in CloudWatch Log Stream: ${context.logStreamName}`,
     PhysicalResourceId: context.logStreamName,
     StackId: event.StackId,
@@ -39,13 +45,17 @@ const submit = async (event, context, success) => {
   }
 };
 
-module.exports = (fn) => wrap(async (event, context) => {
+module.exports = (fn, opts = {}) => wrap(async (event, context) => {
+  Joi.assert(opts, Joi.object().keys({
+    errorOnInvalidEvent: Joi.boolean().optional()
+  }));
+  const errorOnInvalidEvent = get(opts, 'errorOnInvalidEvent', true);
   try {
     await fn(event, context);
   } catch (err) {
     logger.error(`Failure in custom code run inside of lambda-cfn-hook: ${err}`);
-    await submit(event, context, false);
+    await submit(event, context, { success: false, errorOnInvalidEvent });
     throw err;
   }
-  await submit(event, context, true);
+  await submit(event, context, { success: true, errorOnInvalidEvent });
 });
